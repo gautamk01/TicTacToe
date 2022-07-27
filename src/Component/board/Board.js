bc0f6050333055d852b6c6e605f7d185be80c2ce
Gautam Krishna M <charmingdeveloper@protonmail.com>
2022-07-27 11:48:49 +0100

    Now the table is more refined and Bug fixs

--- src/Component/board/Board.js
+++ src/Component/board/Board.js
@@ -2,30 +2,32 @@ import { useState } from "react";
 import Square from "./square";
 
 function Board() {
-  const [arr, setarr] = useState(new Array(9).fill("_"));
+  const [arr, setarr] = useState(new Array(9).fill());
+  const [isnext, setIsnext] = useState(false);
+
   const squareBox = (position) => {
     return (
       <Square
         value={arr[position]}
         click={() => {
+          setIsnext(!isnext);
           setarr(
             arr.map((item, pos) => {
               if (pos === position) {
-                return "x";
-              } else {
-                return item;
+                return isnext ? "O" : "X";
               }
+              return item;
             })
           );
-          console.log();
         }}
       />
     );
   };
+
   return (
-    <div>
+    <div className="MainGameBox">
       <div className="columnBox">
-        {squareBox(0, arr)}
+        {squareBox(0)}
         {squareBox(1)}
         {squareBox(2)}
       </div>
--- src/Component/board/square.js
+++ src/Component/board/square.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Square(props) {
   return (
-    <button className="btn" onClick={props.click}>
-      <h1>{props.value}</h1>
-    </button>
+    <div className="btn" onClick={props.click}>
+      <h1 className="mainKey">{props.value}</h1>
+    </div>
   );
 }
